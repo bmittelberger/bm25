c48e0b6c868a59a03863960b796b22a5175f52e8
Matthew Lamm <matthew.r.lamm@gmail.com>
2016-05-12 21:26:34 -0700

    Coded up BM25, need to figure out if it works.

--- src/edu/stanford/cs276/AScorer.java
+++ src/edu/stanford/cs276/AScorer.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiFunction;
+//import java.util.function.BiFunction;
 
 /**
  * An abstract class for a scorer. 
--- src/edu/stanford/cs276/BM25Scorer.java
+++ src/edu/stanford/cs276/BM25Scorer.java
@@ -32,7 +32,7 @@ public class BM25Scorer extends AScorer {
 	
 	double k1 = 0.1;
 	double pageRankLambda = 0.1;
-	double pageRankLambdaPrime = 0.1;
+	double pageRankLambdaPrime = 0.1; // for the log function: add log( pageRankLambdaPrime + page_rank_doc ) to each relevance computation
 	
 	// query -> url -> document
 	Map<Query,Map<String, Document>> queryDict; 
@@ -58,31 +58,123 @@ public class BM25Scorer extends AScorer {
 			this.calcAverageLengths();
 		}
 
-  /**
+	/**
     * Set up average lengths for BM25, also handling PageRank.
     */
 	public void calcAverageLengths() {
-		lengths = new HashMap<Document,Map<String,Double>>();
-		avgLengths = new HashMap<String,Double>();
-		pagerankScores = new HashMap<Document,Double>();
+		lengths = new HashMap<Document,Map<String,Double>>();	// 
+		avgLengths = new HashMap<String,Double>();				// map from field Strings to a Double ( average number of terms in a field over entire corpus )
+		pagerankScores = new HashMap<Document,Double>();		// the individual page rank scores associated with each document 
+		Map<String,ArrayList<Double>> allLengths = new HashMap<String,ArrayList<Double>>();
 		
-		/*
-		 * TODO : Your code here
-     *        Initialize any data structures needed, perform
-     *        any preprocessing you would like to do on the fields,
-     *        handle pagerank, accumulate lengths of fields in documents.
-     *        
-		 */
-		
-		for (String tfType : this.TFTYPES) {
-			/*
-			 * TODO : Your code here
-       *        Normalize lengths to get average lengths for
-       *        each field (body, url, title, header, anchor)
-       *        using the training set (PA1 corpus).
-			 */
+		for ( String tfType : this.TFTYPES ) { 
+			allLengths.put( tfType , new ArrayList<Double>() );
 		}
+		
+		for ( Query query : this.queryDict.keySet() ){
+			Map<String, Document> documentMap = this.queryDict.get( query );
+			for ( String docString : documentMap.keySet() ){
+				Document doc = documentMap.get( docString );
+				pagerankScores.put( doc , Double.valueOf( (double) doc.page_rank ) );
+				for ( String tfType : this.TFTYPES ){
+					double fieldLength = getFieldLength( tfType , doc );
+					if ( ! lengths.containsKey( doc ) ){
+						Map <String,Double> fieldLengths = new HashMap<String,Double>();
+						fieldLengths.put( tfType , Double.valueOf( fieldLength ) );
+						lengths.put( doc , fieldLengths );
+					}
+					else{
+						Map<String,Double> fieldLengths = lengths.get( doc );
+						fieldLengths.put( tfType , Double.valueOf( fieldLength ) );
+						lengths.put( doc , fieldLengths );
+					}
+					allLengths.get( tfType ).add( Double.valueOf( fieldLength ) );			
+				}
+			}
+		}
+		
+		for ( String tfType : allLengths.keySet() ){
+			Double total = 0.0 ;
+			for ( Double length : allLengths.get( tfType ) ){
+				total += length; 
+			}
+			Double average = total / (double) allLengths.get( tfType ).size();
+			avgLengths.put( tfType , average );
+		}
+		
 
+	}
+	
+	private double getWFieldWeight( String type ) {
+		if (type.equals("url")) {
+			return urlweight;
+		} else if (type.equals("title")) {
+			return titleweight;
+		}else if (type.equals("header")) {
+			return headerweight;
+		}else if (type.equals("body")) {
+			return bodyweight;
+		}else { //anchor
+			return anchorweight;
+		}
+		
+	}
+	
+	private double getBFieldWeight( String type ){
+		if (type.equals("url")) {
+			return burl;
+		} else if (type.equals("title")) {
+			return btitle;
+		}else if (type.equals("header")) {
+			return bheader;
+		}else if (type.equals("body")) {
+			return bbody;
+		}else { //anchor
+			return banchor;
+		}
+		
+	}
+	
+	private double getFieldLength( String type,  Document doc ) {
+
+		if (type.equals("url")) {
+			String[] urlWords = doc.url.split( "[/_\\-.]" );
+			Double urlLength = Double.valueOf( (double) urlWords.length );
+			return urlLength; 
+		} else if (type.equals("title")) {
+			String[] titleWords = doc.title.split( " " );
+			Double titleLength = Double.valueOf( (double) titleWords.length );
+			return titleLength;
+		}else if (type.equals("header")) {
+			List<String> headers = doc.headers;
+			Double headersLength = 0.0;
+			if (headers != null) {
+				for ( String header : headers ){
+					headersLength += (double) header.split( " " ).length;
+				}
+			}
+			return headersLength;
+		}else if (type.equals("body")) {
+			Map<String,List<Integer>> bodyHits = doc.body_hits;
+			Double bodyLength = 0.0; // if bodyHits null then return the default 0.0 value
+			if ( bodyHits != null ){
+				for ( String term : bodyHits.keySet() ){
+					bodyLength += (double) bodyHits.get( term ).size(); 
+				}
+			}
+			return bodyLength;
+		}else { //anchor
+			Map<String, Integer> anchors = doc.anchors;
+			Double anchorsLength = 0.0;
+			if ( anchors != null) {
+				for ( String anchor : anchors.keySet() ){
+					double anchorCount = anchors.get( anchor ).doubleValue() ;
+					double textLength = (double) anchor.split( " " ).length;
+					anchorsLength += Double.valueOf( textLength * anchorCount );
+				}
+			}
+			return anchorsLength;
+		}
 	}
 
   /**
@@ -99,9 +191,45 @@ public class BM25Scorer extends AScorer {
 		
 		/*
 		 * TODO : Your code here
-     *        Use equation 5 in the writeup to compute the overall score
-     *        of a document d for a query q.
+		 *        Use equation 5 in the writeup to compute the overall score
+		 *        of a document d for a query q.
 		 */
+		Map<String,Double> wdt = new HashMap<String,Double>();
+		
+		// compute equation four values , the weight sums of BM25 field-relative word frequencies
+		for ( String word : q.queryWords ){
+			
+			for ( String tfType : this.TFTYPES ){
+				Double weight = getWFieldWeight( tfType );
+				
+				if ( tfs.get( tfType ).containsKey( word ) ) {
+					if ( ! wdt.containsKey( word  ) ){
+						wdt.put( word , weight*tfs.get( tfType ).get( word ) );
+					}
+					else{
+						Double currentWeight = wdt.get( word );
+						wdt.put( word , weight*tfs.get( tfType ).get( word ) + currentWeight );
+					}
+				}	
+			}
+		}
+		
+		for ( String word : wdt.keySet() ){
+			Double idf = 0.0;
+			if ( this.idfs.containsKey( word ) ){
+				idf = this.idfs.get( word );
+			}
+			else{
+				idf = this.idfs.get( "$NICKI_MINAJ_NOT$" );
+			}
+			Double numerator = wdt.get( word );
+			Double denominator = k1 + wdt.get( word );
+			score += ( numerator / denominator ) * idf ;
+		}
+		
+		// add the page rank final constant 
+		score += pageRankLambda * Math.log( pageRankLambdaPrime  + d.page_rank );
+		
 		
 		return score;
 	}
@@ -112,13 +240,31 @@ public class BM25Scorer extends AScorer {
     * @param d the Document
     * @param q the Query
     */
-	public void normalizeTFs(Map<String,Map<String, Double>> tfs,Document d, Query q) {
+	public void normalizeTFs(Map<String,Map<String, Double>> tfs , Document d , Query q) {
 		/*
 		 * TODO : Your code here
-     *        Use equation 3 in the writeup to normalize the raw term frequencies
-     *        in fields in document d.
+		 *        Use equation 3 in the writeup to normalize the raw term frequencies
+		 *        in fields in document d.
 		 */
+		// matt note : tfs is already relative to the specific document d 
+		
+		for ( String tfType : this.TFTYPES ){
+			Map<String,Double> termCounts = tfs.get( tfType );
+			
+			Double averageFieldLength = avgLengths.get( tfType );
+			Double docFieldLength = lengths.get( d ).get( tfType );
+			double fieldWeight = getBFieldWeight( tfType );
+			Double lengthDiv = docFieldLength / averageFieldLength;
+			Double denominator = ( 1.0 + fieldWeight*( lengthDiv - 1 ) );
+			
+			for ( String term : termCounts.keySet() ){
+				termCounts.put( term , termCounts.get( term ) / denominator );
+			}
+		}
+		
 	}
+	
+	
 	
 	
 	/**
